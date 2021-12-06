e99becd2d6268c01dff8481ce8247134a7d88c32
Manuel Thalmann <m@nuth.ch>
2021-12-06 09:21:17 +0100

    Create a Uno application

---
+++ Sourcerer.Wasm/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Windows.UI.Xaml;
+
+namespace Sourcerer.Wasm
+{
+	public class Program
+	{
+		private static App _app;
+
+		static int Main(string[] args)
+		{
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+			return 0;
+		}
+	}
+}
